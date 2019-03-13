a519cd285b238e19513ef4d4ec01e96bd35a96ce
Alex <Jillbk22@gmail.com>
2019-03-13 08:56:54 -0400

    reworded some qs to return strings

--- script.js
+++ script.js
@@ -15,26 +15,53 @@ console.log(regText1('hello'))
 //the key is with either the duck or the goose! write a regEx that matches 'duck' OR 'goose'
 
 regText2 = (string) => {
-  myString = "the farm is full of mysterious animals who look like they're hiding something. Could it be the duck? The pig? The cow? the goose!?"
+  myString = "The farm is full of mysterious animals who look like they're hiding something. Could it be the octopus? The pig? The cow? the goose!?"
+
+  myString2 = "The farm is full of mysterious animals who look like they're hiding something. Could it be the duck? The pig? The cow? the rabbit!?"
+
+  myString3 = "The farm is full of mysterious animals who look like they're hiding something. Could it be the chick? The pig? The cow? the rabbit!?"
+
   let newRegEx = new RegExp(string)
-  return newRegEx.test(myString)
+
+  let answer1 = newRegEx.test(myString)
+  let answer2 = newRegEx.test(myString2)
+  let answer3 = newRegEx.test(myString3)
+
+  if(answer1 && answer2 && !answer3){
+    return 'Follow that goose!'
+  }
+  else { return 'Follow that duck!'}
 }
 
 console.log(regText2('goose|duck'))
-//returns true
+//returns Follow that goose!
 
 //==================================
 
-// The dastardly criminals have intercepted our message and changed the cases. Ugh it's so difficult to read now! Is there a clue? Write a regex that searches for the string clue in any case.
+// The dastardly criminals have intercepted our message and changed the cases. Ugh it's so difficult to read now! Is there a clue? Write a regex that searches for the string 'clue' in any case.
 
 regText3 = (string, flag) => {
-  myString = "wHERe iS THe cLeW is the CLu HEre? hOW abOUt THis klUE Is ThIs the CEul? i bET That'S tHe CLuUe oVEr thERe. OR Is tHe clUE heRE? WHerE iS tHAt DarN cIue?"
+  myString1 = "wHERe iS THe cLeW is the CLu HEre? hOW abOUt THis klUE Is ThIs the CEul? i bET That'S tHe CLuUe oVEr thERe. OR Is tHe clLUE heRE? WHerE iS tHAt DarN cIue?"
+
+  myString2 = "wHERe iS THe cLeW is the CLu HEre? hOW abOUt THis klUE Is ThIs the CEul? i bET That'S tHe CLuUe oVEr thERe. OR Is tHe clUE heRE? WHerE iS tHAt DarN cIue?"
+
+  myString3 = "wHERe iS THe cLeW is the CLu HEre? hOW abOUt THis klUE Is ThIs the CEul? i bET That'S tHe CLuUe oVEr thERe. OR Is tHe cleW heRE? WHerE iS tHAt DarN cIue?"
+
   let newRegEx = new RegExp(string,[flag])
-  return newRegEx.test(myString)
+
+  let answer1 = newRegEx.test(myString1)
+  let answer2 = newRegEx.test(myString2)
+  let answer3 = newRegEx.test(myString3)
+
+  if(answer2 && !answer1 && !answer3){
+    return "the clue is leading to the men's wearhouse"
+  } else {
+    return "the clue is leading to a live mouse!"
+  }
 }
 
 console.log(regText3('clue', 'i'))
-//returns true
+//returns the clue is leading to the men's wearhouse
 
 //==================================
 
@@ -53,14 +80,18 @@ console.log(regText4('crime'))
 // The street address of the safehouse is the number of knives in this message. How many knives are there here?
 
 regText4 = (string, flag) => {
-  myString = "knif gun candlestick candestick knife gun bomb cannon knife musket pistol knipe killer bee knife"
+  myString = "knif gun candlestick candestick knife gun bomb cannon knife musket knife pistol knipe killer bee knife"
   let newRegEx = new RegExp(string,[flag])
-  return myString.match(newRegEx)
+  let answer = myString.match(newRegEx)
+  if(answer.length === 4){
+    return 'go to the boardwalk'
+  } else {
+    return 'go to the docks!'
+  }
 }
 
 console.log(regText4('knife', 'g'))
-// returns ["knife", "knife", "knife"]
-// choose between 4 knivey Way, 1 Knife Alley, 3 KnifeTown USA
+// returns 'go to the boardwalk'
 
 //==================================
 
@@ -69,11 +100,16 @@ console.log(regText4('knife', 'g'))
 regText5 = (string, flag) => {
   myString = "Well well well, looks like we've found a hat"
   let newRegEx = new RegExp(string)
-  return myString.match(newRegEx)
+  let answer = myString.match(newRegEx)
+  if(answer[0] === 'hat'){
+    return 'just a hat, looks like this is a dead lead. Go back to the office.'
+  } else {
+    return 'Oh no! They found your source, go check up on him.'
+  }
 }
 
 console.log(regText5('.at'))
-//returns ["hat", index: 41, input: "Well well well, looks like we've found a hat", groups: undefined]
+//returns 'just a hat, looks like this is a dead lead. Go back to the office.'
 
 //==================================
 
@@ -83,11 +119,16 @@ console.log(regText5('.at'))
 regText6 = (string, flag) => {
   myString = "Crime is my passion! I like to live, laugh, love and lime. And by lime of course I mean crime!"
   let newRegEx = new RegExp(string, flag)
-  return myString.match(newRegEx)
+  let answer = myString.match(newRegEx)
+  if(answer.length === 31){
+    return 'Head to the dump!'
+  } else {
+    return 'Go to the roof!'
+  }
 }
 
 console.log(regText6('[aeiou]', 'ig'))
-//returns ["i", "e", "i", "a", "i", "o", "I", "i", "e", "o", "i", "e", "a", "u", "o", "e", "a", "i", "e", "A", "i", "e", "o", "o", "u", "e", "I", "e", "a", "i", "e"]
+//returns 'Head to the dump!'
 
 
 //==================================
@@ -96,7 +137,7 @@ console.log(regText6('[aeiou]', 'ig'))
 //Ok gumshoe, time to break some codes! The criminals have hidden the location of their hideout by only using only capital letters. Search the string for capital letters.
 
 regText6 = (string, flag) => {
-  myString = "They'll never figure out the address to our secret location. How could they? Everyone know's they're dum-dums. Damn their good looks though, UM what did i just say? forget that they're ugly too. Peace out."
+  myString = "They'll never figure out the address to our secret location. How could they? Everyone know's they're dum-dums. Damn their good looks though, UM what did i just say? forget that they're ugly too! Peace out."
   let newRegEx = new RegExp(string, flag)
   return myString.match(newRegEx)
 }
@@ -107,14 +148,15 @@ console.log(regText6('[A-Z]', 'g'))
 //==================================
 
 
-// Oh shoot! Our wire cut out halfway through the confession. Can you find out what the criminal said before " I commited" and "I hope I don't get arrested!"
+// Oh shoot! Our wire cut out halfway through the confession. Can you find out what the criminal said after " I commited" and before "I hope I don't get arrested!"
 
 regText7 = (string, flag) => {
   myString = "Oops I cut my finger, so much blood! It's like that time I commited murder. I hope I don't get arrested! My girlfriend would be so mad, we're in a very commited relationship."
   let newRegEx = new RegExp(string, flag)
-  console.log(newRegEx)
   return myString.match(newRegEx)
 }
 
 console.log(regText7("I commited[a-z .!?]*I hope I don't get arrested!", "ig"))
 //returns ["I commited murder. I hope I don't get arrested!"]
+
+//tabs vs spaces would be a good challege
